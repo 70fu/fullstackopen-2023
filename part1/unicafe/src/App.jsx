e8e030daa1cc0097c7bac9c3dd6878b0378eef43
Simon Reiser <ein.simonreiser@gmail.com>
2023-09-03 17:55:25 +0900

    complete exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,14 +6,20 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Statistics = ({good,neutral,bad}) =>(
-  <div>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-)
+const Statistics = ({good,neutral,bad}) => {
+  const all = good + neutral + bad;
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all===0?0:(good-bad)/all}</p>
+      <p>positive {all===0?0:good*100.0/all} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
